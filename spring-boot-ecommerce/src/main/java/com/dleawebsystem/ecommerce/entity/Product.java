a8ea87dcbf97bdc94cf3a3cfac15505b057b634f
EudesGomes017 <eudesgomes017@gmail.com>
2024-04-28 22:45:15 -0300

    Create request frontend end backend

--- spring-boot-ecommerce/src/main/java/com/dleawebsystem/ecommerce/entity/Product.java
+++ spring-boot-ecommerce/src/main/java/com/dleawebsystem/ecommerce/entity/Product.java
@@ -1,7 +1,7 @@
 package com.dleawebsystem.ecommerce.entity;
 
 import java.math.BigDecimal;
-import java.time.Instant;
+import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -17,46 +17,45 @@ import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "product")
+@Table(name="product")
 @Data
 public class Product {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-	@ManyToOne
-	@JoinColumn(name = "category_id", nullable = false)
-	private ProductCategory category;
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
 
-	@Column(name = "sku")
-	private String sku;
+    @Column(name = "sku")
+    private String sku;
 
-	@Column(name = "name")
-	private String name;
+    @Column(name = "name")
+    private String name;
 
-	@Column(name = "description")
-	private String description;
+    @Column(name = "description")
+    private String description;
 
-	@Column(name = "uni_Price")
-	private BigDecimal uniPrice;
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
 
-	@Column(name = "image_Url")
-	private String imageUrl;
+    @Column(name = "image_url")
+    private String imageUrl;
 
-	@Column(name = "active")
-	private boolean active;
+    @Column(name = "active")
+    private boolean active;
 
-	@Column(name = "units_In_Stock")
-	private int unitsInStock;
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
 
-	@Column(name = "date_Created")
-	@CreationTimestamp
-	private Instant dateCreated;
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
 
-	@Column(name = "last_Updated")
-	@UpdateTimestamp
-	private Instant lastUpdated;
-
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
 }
